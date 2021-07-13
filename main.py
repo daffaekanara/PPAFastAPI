858c1b5cd667bae4aeeae4d45fa54517f67790e5
BuzzDyne <buzzdyne@gmail.com>
2021-07-13 16:17:14 +0700

    Implemented Middleware Origins

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from routers import division, employee, auth, training, trainingTarget, debug, socialContrib, attrition, engagement
 from database import engine
@@ -6,6 +7,13 @@ from database import engine
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+origins = [
+    "https://111.95.148.87"
+]
+
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
 # app.include_router(debug.router)
 app.include_router(engagement.router)
 app.include_router(attrition.router)
