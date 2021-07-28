98dc85e9e6575407f9e65717a04191de16fbee55
BuzzDyne <buzzdyne@gmail.com>
2021-07-28 17:07:42 +0700

    Implemented Age Group and Edu Level Endpoints

--- app/routers/dashboard.py
+++ app/routers/dashboard.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 import datetime
+from dateutil.relativedelta import relativedelta
 import schemas, utils
 from models import *
 from database import get_db
@@ -18,7 +19,7 @@ gen_y_youngest = datetime.date(1989,12,31)
 
 # API
 @router.get('/api/age_group/')
-def get_header_training(db: Session = Depends(get_db)):
+def get_age_group(db: Session = Depends(get_db)):
     # Get All Emps
     emps = db.query(Employee).all()
 
@@ -49,5 +50,94 @@ def get_header_training(db: Session = Depends(get_db)):
                 res[2]['male_sum'] += 1
             elif e.gender == 'F':
                 res[2]['female_sum'] += 1
+
+    return res
+
+@router.get('/api/education_level/')
+def get_edu_level(db: Session = Depends(get_db)):
+    # Get All Emps
+    emps = db.query(Employee).all()
+
+    titles = ["Management/Economy", "Information Technology", "Others"]
+    res = []
+
+    # Init result dict
+    for title in titles:
+        res.append({
+            "bachelor_sum"  : 0,
+            "master_sum"    : 0,
+            "major_title"   : title
+        })
+    
+    for e in emps:
+        if e.edu_category == "Information Technology":
+            if e.edu_level == "Bachelor":
+                res[1]['bachelor_sum'] += 1
+            elif e.edu_level == "Master":
+                res[1]['master_sum'] += 1
+        elif e.edu_category == "Management/Economy":
+            if e.edu_level == "Bachelor":
+                res[0]['bachelor_sum'] += 1
+            elif e.edu_level == "Master":
+                res[0]['master_sum'] += 1
+        elif e.edu_category == "Others":
+            if e.edu_level == "Bachelor":
+                res[2]['bachelor_sum'] += 1
+            elif e.edu_level == "Master":
+                res[2]['master_sum'] += 1
+
+    return res
+
+@router.get('/api/audit_exp/')
+def get_total_audit_exp(db: Session = Depends(get_db)):
+    # Get All Emps
+    emps = db.query(Employee).all()
+
+    # in_uob, outside_uob, total_exp, year
+    year_cats = [
+        "Less than 3", 
+        "3-6", 
+        "7-9",
+        "10-12",
+        "13-15",
+        "More Than 15"
+    ]
+    res = []
+
+    # Init result dict
+    for y in year_cats:
+        res.append({
+            "in_uob"        : 0,
+            "outside_uob"   : 0,
+            "total_exp"     : 0,
+            "year"          : y
+        })
+    
+    for e in emps:
+        print()
+
+        years   = [0,0,0] #in UOB, outside UOB, total
+        keys    = ["in_uob", "outside_uob", "total_exp"]
+
+        years[0]    = relativedelta(datetime.date.today(), e.date_first_uob).years
+        years[1]    = e.year_audit_non_uob
+        years[2]    = years[0] + years[1]
+
+        print()
+
+        for index, year in enumerate(years):
+            if year < 3:
+                res[utils.find_index(res,"year","Less than 3")][keys[index]] += 1
+            elif year < 7:
+                res[utils.find_index(res,"year","3-6")][keys[index]] += 1
+            elif year < 10:
+                res[utils.find_index(res,"year","7-9")][keys[index]] += 1
+            elif year < 13:
+                res[utils.find_index(res,"year","10-12")][keys[index]] += 1
+            elif year < 16:
+                res[utils.find_index(res,"year","13-5")][keys[index]] += 1
+            else:
+                res[utils.find_index(res,"year","More than 15")][keys[index]] += 1
+
 
     return res
