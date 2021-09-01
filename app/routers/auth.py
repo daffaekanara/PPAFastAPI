5f5b3d0b0f83ba238a3ff67c85a937054e1d50ab
BuzzDyne <buzzdyne@gmail.com>
2021-09-01 13:39:08 +0700

    Fix divison 'name' to 'short_name'

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -14,10 +14,10 @@ def login(req: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(data
     user = db.query(models.Employee).filter(models.Employee.email == req.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Email not found!")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Email not found!")
 
     if not hashing.verify_bcrypt(user.pw, req.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect password")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Incorrect password")
 
     # Generate JWT Token
     access_token = JWTtoken.create_access_token(data={"sub": user.email})
@@ -31,7 +31,7 @@ def login_v1(req: schemas.TempLoginForm, db: Session = Depends(database.get_db))
     ).first()
 
     if not user or not hashing.verify_bcrypt(user.pw, req.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect email or password!")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Incorrect email or password!")
 
     token_payload = {
         "id"    : str(user.id),
@@ -39,7 +39,7 @@ def login_v1(req: schemas.TempLoginForm, db: Session = Depends(database.get_db))
         "email" : user.email,
         "name"  : user.name,
         "role"  : user.role.name,
-        "div"   : user.part_of_div.name
+        "div"   : user.part_of_div.short_name
     }
 
     # Generate JWT Token
