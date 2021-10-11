4e90ab694477a390ebd9fb523e41e337aff24a8a
daffaekanara <87791137+daffaekanara@users.noreply.github.com>
2021-10-11 10:37:45 +0700

    fix IM002 Error

--- app/database.py
+++ app/database.py
@@ -1,10 +1,26 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DB_URL = 'sqlite:///data/database.db'
+#sqlite configuration
 
-engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread":False})
+#SQLALCHEMY_DB_URL = 'sqlite:///data/database.db'
+
+#engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread":False})
+
+#-------------------------------------------------------------------------------------
+#SQL Server configuration
+
+SERVER = 'DESKTOP-RR8SR4K'
+DATABASE = 'master'
+DRIVER = 'ODBC Driver 17 for SQL Server'
+DATABASE_CONNECTION = f'mssql://@{SERVER}/{DATABASE}?driver={DRIVER}'
+
+#Server=localhost\MSSQLSERVER01;Database=master;Trusted_Connection=True;
+
+engine = create_engine(DATABASE_CONNECTION, connect_args={"check_same_thread":False})
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
